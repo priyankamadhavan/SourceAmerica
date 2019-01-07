7be050cf66b142592ef3795166d17686e8f538b1
ic19 <46301105+ic19@users.noreply.github.com>
2019-01-06 20:32:09 -0500

    Add files via upload

---
+++ src/main/java/com/example/ic/motivateme/Failure.java
@@ -0,0 +1,28 @@
+package com.example.ic.motivateme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Failure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failure);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStartScreen();
+            }
+        });
+    }
+
+    public void openStartScreen(){
+        Intent intent = new Intent(this,StartScreen.class);
+        startActivity(intent);
+    }
+}
